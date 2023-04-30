2c522ad063bc6cd09cbb30b46149b62fb9cf819d
Saeed Mogharian <moghariansaeed@gmail.com>
2023-04-30 12:34:14 +0330

    first page implementation init

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,80 @@
 from django.db import models
 
 # Create your models here.
+
+from accounts.models import User
+from typing import List
+from django.db import models
+
+
+class Employer(models.Model):
+    """person who can create tasks."""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Contractor(models.Model):
+    """person who can accept and do the tasks"""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Task(models.Model):
+    class TaskStatus(models.TextChoices):
+        PENDING = "P", "تعریف شده"
+        ASSIGNED = "A", "واگذار شده"
+        DONE = "D", "انجام شده"
+
+    title = models.CharField(max_length=60)
+    owner = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    state = models.CharField(
+        max_length=1,
+        default=TaskStatus.PENDING,
+        choices=TaskStatus.choices,
+    )
+    cost = models.PositiveIntegerField()
+    time_period = models.PositiveSmallIntegerField()
+    description = models.TextField(blank=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    assigned_contractor = models.ForeignKey(
+        Contractor,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    @property
+    def is_assigned(self) -> bool:
+        return self.assigned_contractor is not None
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def get_all_data_to_show(cls) -> List:
+        """return tasks data for previewing"""
+        result = []
+        tasks = list(cls.objects.all().order_by("-created_at").values())
+
+        for task in tasks:
+            owner = str(Employer.objects.get(pk=task.get("owner_id")))
+            state = Task.objects.get(id=task.get("id")).get_state_display()
+            task["owner"] = owner
+            task["state"] = state
+            result.append(task)
+        return result
+
+    def assign_contractor(self, contractor: Contractor) -> None:
+        self.assigned_contractor = contractor
+        self.state = Task.TaskStatus.ASSIGNED
+        self.save()
+
+    def done(self) -> None:
+        self.state = Task.TaskStatus.DONE
+        self.save()
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from .models import Task, Contractor, Employer
+# from .forms import SignupForm
+
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
+from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.urls import reverse
+import logging
+
 
 # Create your views here.
+
+def index(request):
+    """main page for displaying tasks"""
+
+    # read last page number visited by user from cookies
+    tasks = Task.objects.all().order_by("-created_at")
+    # paginator = Paginator(tasks, PAGINATION_PER_PAGE)
+    # page = paginator.get_page(page_number)
+    # context = {
+    #     "task_page": page,
+    #     "user": request.user,
+    #     "form": SignupForm(),
+    #     "user_type": "کارفرما" if hasattr(request.user, "employer") else "پیمانکار",
+    #     "is_employer": hasattr(request.user, "employer"),
+    # }
+
+    response = render(request, "index.html")
+    # response.set_cookie("page_number", str(page_number))
+    return response
